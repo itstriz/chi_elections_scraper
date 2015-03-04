029e60e1715af5b16f9d056afecd2bea7b990f3d
Matt Johnson <mjohn23@gmail.com>
2015-03-04 14:52:40 -0600

    Checking screenshot

--- scrape.js
+++ scrape.js
@@ -2,11 +2,8 @@ var $ = require('jquerygo');
 $.config.AddJQuery = false;
 
 $.visit('http://www.chicagoelections.com/en/election3.asp', function() {
-    $('[name=D3]').val('2015 Municipal General - 2/24/15                   ', function() {
-        $('input[type=submit]').click(function() {
-            $.waitForPage(function() {
-                $.capture(__dirname + '/screenshot.png');
-            });
-        });
+    $('[name=D3]').val('2015 Municipal General - 2/24/15                  ', function() {
+        $.capture(__dirname + '/screenshot.png');
     });
+    $.close();
 });
