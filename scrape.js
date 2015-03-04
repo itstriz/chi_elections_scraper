42116ae30c6ab5d511e794537ab45ed153f9e946
Matt Johnson <mjohn23@gmail.com>
2015-03-04 14:39:56 -0600

    Setup using jquerygo

--- scrape.js
+++ scrape.js
@@ -1,2 +1,12 @@
-var request = require('request');
-var cheerio = require('cheerio');
+var $ = require('jquerygo');
+$.config.AddJQuery = false;
+
+$.visit('http://www.chicagoelections.com/en/election3.asp', function() {
+    $('[name=D3]').val('2015 Municipal General - 2/24/15                   ', function() {
+        $('input[type=submit]').click(function() {
+            $.waitForPage(function() {
+                $.capture(__dirname + '/screenshot.png');
+            });
+        });
+    });
+});
