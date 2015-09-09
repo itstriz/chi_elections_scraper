79ad9980e7b939fb91ff386e47687a09aae8c9ff
Matt Johnson <mjohn23@gmail.com>
2015-09-09 10:34:00 -0500

    Update .gitignore

--- scrape.js
+++ scrape.js
@@ -1,9 +1,18 @@
+var async = require('async');
 var $ = require('jquerygo');
-$.config.AddJQuery = false;
 
-$.visit('http://www.chicagoelections.com/en/election3.asp', function() {
-    $('[name=D3]').val('2015 Municipal General - 2/24/15                  ', function() {
+$.config.site = 'http://www.chicagoelections.com';
+
+async.series([
+    $.go('visit','/en/election3.asp'),
+    $('[name=D3]').go('val', '2015 Municipal General - 2/24/15                  '),
+    $('input[type=submit]').go('click'),
+    $.go('waitForPage'),
+    function(done) {
         $.capture(__dirname + '/screenshot.png');
-    });
+        done();
+    }
+], function() {
+    console.log('Finished');
     $.close();
 });
